6928845475c48f5ef10f85c0d2db9286af9a97a4
shanesirohi7 <shanesirohi@gmail.com>
2024-04-05 23:58:03 +0530

    newsss

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const publicDirectoryPath = path.join(__dirname, 'public');
 app.use(express.static(publicDirectoryPath));
 
 // Start the server
-const server = app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on port ${server.address().port}`);
+const PORT = process.env.PORT || 10000; // Use the specified port or default to 10000
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
