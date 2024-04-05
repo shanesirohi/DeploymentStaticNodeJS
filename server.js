0b1c9b9ea0335afb67a7af626f7c37720bb38ca5
shanesirohi7 <shanesirohi@gmail.com>
2024-04-06 00:00:16 +0530

    ew

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const publicDirectoryPath = path.join(__dirname, 'public');
 // Serve static files from the public directory
 app.use(express.static(publicDirectoryPath));
 
+// Serve index.html for the root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 // Start the server
 const PORT = process.env.PORT || 10000; // Use the specified port or default to 10000
 app.listen(PORT, '0.0.0.0', () => {
